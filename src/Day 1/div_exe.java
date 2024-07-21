06d4a212db6c962f03d8d934d8fee085096db47c
Raj <connect.rajtrivedi@gmail.com>
2024-07-21 17:46:55 +0530

    Day 1

---
+++ src/Day 1/div_exe.java
@@ -0,0 +1,8 @@
+public class div_exe {
+    public static void main(String[] args) {
+        int n1=20, n2=3;
+        float res =  (float) n1/n2;
+
+        System.out.println("Result of Float Division is:"+res);
+    }
+}
